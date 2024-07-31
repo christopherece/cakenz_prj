ef478f51e9b68bd194646fbf481c4f19b8e55d87
christopherece <christopheranchetaece@gmail.com>
2024-07-31 21:46:51 +1200

    Added Static Files

--- cakenz/cakenz/settings.py
+++ cakenz/cakenz/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'cakenz.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Pacific/Auckland'
 
 USE_I18N = True
 
@@ -115,9 +116,37 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'cakenz/static')
+]
+
+# Media Foder Settings 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Messagse
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.SUCCESS: 'success',
+    messages.ERROR: 'danger'
+
+}
+
+#Email Config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'noreply.ekepanuku@gmail.com'
+EMAIL_HOST_PASSWORD = 'piwnvunwuoudyibr'
+EMAIL_USE_TLS = True
+
+# try:
+#     from .local_settings import *
+# except ImportError:
+#     pass
--- cakenz/pages/views.py
+++ cakenz/pages/views.py
@@ -1,3 +1,12 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+
+
 from django.shortcuts import render
+from django.http import JsonResponse
+
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'pages/index.html')
